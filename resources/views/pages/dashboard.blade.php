1bee900f2af6c526278231515c3887949650268a
ilyasam357 <ilyasam357@gmail.com>
2022-08-11 13:50:15 +0700

    bismillah

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.main')
+
+@section('content')
+<div class="content">
+    <div class="card mx-4 my-4" style="width:20rem;">
+     <h2>testing</h2>
+
+
+    
+    </div>
+</div>
+@endsection
